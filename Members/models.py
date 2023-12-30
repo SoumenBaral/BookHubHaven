f0c766724b832ee0725e5951565ccf02a1591c2b
SoumenBaral <shumen.sb@gmail.com>
2023-12-30 19:07:09 +0600

    add Account model and register it

--- Members/models.py
+++ Members/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from decimal import Decimal
+class MembersAccount(models.Model):
+    user = models.OneToOneField(User,related_name ="account",on_delete=models.CASCADE)
+    account_no = models.IntegerField(unique = True)
+    birth_date = models.DateField(null = True,blank = True)
+    initial_deposit_date = models.DateField(auto_now_add = True)
+    balance = models.DecimalField(max_digits=12, decimal_places=2,default=Decimal('0.00'))
 
-# Create your models here.
+    def __str__(self) -> str:
+        return str(self.account_no)
